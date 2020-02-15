bbbc0a0c14b8ff8219fac75f0c9c4ef2709f7015
kennethvdborne <44980914+kennethvdborne@users.noreply.github.com>
2020-02-15 22:01:03 +0100

    ArtNetReceiver

--- server/src/main/java/lighting/server/ArtNetSender.java
+++ server/src/main/java/lighting/server/ArtNetSender.java
@@ -7,10 +7,20 @@ public class ArtNetSender {
     ArtNetClient artnet = new ArtNetClient();
 
     public void sendData(){
+        byte[] dmxData = new byte[512];
         artnet.start();
-        byte[] data = artnet.readDmxData(0, 0);
-        System.out.println("First Byte: " + data[0] + 0xFF);
+
+
+        dmxData[0] = (byte) 128;
+
+        artnet.unicastDmx("127.0.0.1", 0, 0, dmxData);
+
         artnet.stop();
+        try {
+            Thread.sleep(500000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
 }
