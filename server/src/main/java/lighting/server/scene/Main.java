6585bdcb40c40d76a7e622289207623af1f34712
matthiassomay <matthiassomay@gmail.com>
2020-02-16 18:31:58 +0100

    list of scenes

--- server/src/main/java/lighting/server/scene/Main.java
+++ server/src/main/java/lighting/server/scene/Main.java
@@ -1,10 +1,8 @@
 package lighting.server.scene;
 
-import lighting.server.ArtNetSender;
 import lighting.server.IO.SceneSerialization;
 
 import java.io.IOException;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
