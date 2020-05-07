dd8a80b09f032f22290f903dfbd9322f42d16a0e
matthiassomay <matthiassomay@gmail.com>
2020-05-07 16:06:30 +0200

    directories

--- client/src/app/app-rx-stomp.config.ts
+++ client/src/app/app-rx-stomp.config.ts
@@ -2,7 +2,7 @@ import { InjectableRxStompConfig } from '@stomp/ng2-stompjs';
 
 export const appRxStompConfig: InjectableRxStompConfig = {
 
-  brokerURL: 'wss://127.0.0.1:8080/stomp/websocket',
+  brokerURL: 'wss://localhost:8080/stomp/websocket',
 
   heartbeatIncoming: 0,
   heartbeatOutgoing: 20000,
