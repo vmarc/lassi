f556cc8ef3395555dfeb0cc98f673f406f8508c2
kennethvdborne <44980914+kennethvdborne@users.noreply.github.com>
2020-05-22 11:29:33 +0200

    minor adj

--- server/src/main/java/lighting/server/artnet/ArtnetSender.java
+++ server/src/main/java/lighting/server/artnet/ArtnetSender.java
@@ -101,11 +101,14 @@ public class ArtnetSender {
     }
 
     public void stop() {
-        if (sceneFader != null){
-            sceneFader.setTotalFrames(0);
+        if (stop){
+            if (sceneFader != null){
+                sceneFader.setTotalFrames(0);
+            }
+            stop = true;
+            fadeStop();
+            stop = false;
         }
-        stop = true;
-        fadeStop();
     }
 
     public void fadeStop(){
