5a04873c716f24e9e01714badddff9d96f9022ea
matthiassomay <matthiassomay@gmail.com>
2020-06-01 16:02:18 +0200

    button pages

--- client/src/app/settings/settings.ts
+++ client/src/app/settings/settings.ts
@@ -2,10 +2,12 @@ export class Settings {
 
   framesPerSecond: number;
   fadeTimeInSeconds: number;
+  buttonPages: number;
 
-  constructor(framesPerSecond: number, fadeTimeInSeconds: number) {
+  constructor(framesPerSecond: number, fadeTimeInSeconds: number, buttonPages: number) {
     this.framesPerSecond = framesPerSecond;
     this.fadeTimeInSeconds = fadeTimeInSeconds;
+    this.buttonPages = buttonPages;
   }
 
 }
